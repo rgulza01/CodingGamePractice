3a3020e9ebb53515c3ca17a774a9234a1ad5f76d
rgulza01 <56838325+rgulza01@users.noreply.github.com>
2022-06-16 16:19:39 +0200

    https://www.codingame.com/ide/puzzle/the-descent

---
+++ TheDescent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            List<int> peaks = new List<int>();
+            for (int i = 0; i < 8; i++)
+            {
+                var mountainH = int.Parse(Console.ReadLine()); // represents the height of one mountain.
+                peaks.Add(mountainH);
+                if (i == 7)
+                {
+                    var highest_index = peaks.IndexOf(peaks.Max());
+                    var highest_peak = peaks.Max();
+                    Console.WriteLine();
+                    peaks.Remove(highest_peak);
+                    //peaks.ToList().ForEach(i => Console.WriteLine(i.ToString()));
+                }
+            }
+
+        }
+
+
+    }
+}
